90b442fb6bc7199e6eac63767a22beec014cd68a
Zeifyra <s.rodriguez63@uniandes.edu.co>
2023-11-08 20:04:57 -0500

    first commit

---
+++ Bloque.java
@@ -0,0 +1,74 @@
+// Java implementation for creating 
+// a block in a Blockchain 
+
+import java.util.Date; 
+
+public class Bloque { 
+
+	// Every block contains 
+	// a hash, previous hash and 
+	// data of the transaction made 
+	private String hash; 
+	private String hashprevio; 
+	private String datos; 
+    private int    nonce;
+    private int    id;
+	private long   timeStamp; 
+
+	// Constructor for the block 
+	public Bloque(int id,String datos, String hashprevio) 
+	{ 	
+		this.id    = id;
+		this.datos = datos; 
+		this.hashprevio = hashprevio; 
+		this.timeStamp = new Date().getTime(); 
+		this.hash = calcularHash(); 
+	} 
+
+	// Function to calculate the hash 
+	public String calcularHash() 
+	{ 
+		String calculatedhash = Cripto.sha256(hashprevio + Long.toString(timeStamp) + datos + nonce); 
+
+		return calculatedhash; 
+	}
+	
+	public void printBlock() {
+		System.out.println("==== Bloque " + id + " ====");
+		System.out.println("Hash: " + hash);
+		System.out.println("Hash Previo: " + hashprevio);
+		System.out.println("Datos: " + datos);
+		System.out.println("Nonce: " + nonce);
+		System.out.println("Stamp: " + Long.toString(timeStamp));
+	}
+
+	public void minarBloque(int dificultad) {
+		   String tgt = getDificultad(dificultad); //Create a string with difficulty * "0"
+		   while(!hash.substring( 0, dificultad).equals(tgt)) {
+				 nonce ++;
+		         hash = calcularHash();
+		   }
+		   System.out.println("Bloque minado : " + hash);
+    }
+		//Returns difficulty string target, to compare to hash.
+		//e.g. difficulty of 5 will return "00000"
+		public String getDificultad(int dificultad) {
+			return new String(new char[dificultad]).replace('\0', '0');
+		}
+	public String darHash() {
+		
+		   // completar:
+		   // retornar el valor del hash del bloque
+
+		   String valorHash = hash;
+		   return valorHash;
+	}
+	public String darHashPrevio() {
+		   // completar:
+		   // retornar el valor del hash previo
+		   String valorHashPrevio = hashprevio;
+		   return valorHashPrevio;
+	}
+		
+} 
+
---
+++ Cadena.java
@@ -0,0 +1,92 @@
+/*
+* Este código se basa en código desarrollado por:
+* https://www.geeksforgeeks.org/implementation-of-blockchain-in-java/
+* Modificado por: < Santiago Rodriguez Bernal > < Diego Rodriguez >
+*/
+// Java implementation to store
+// blocks in an ArrayList
+
+import java.util.ArrayList;
+
+public class Cadena {
+
+	// ArrayList to store the blocks
+	public static ArrayList<Bloque> blockchain= new ArrayList<Bloque>();
+    public static int dificultad = 2;
+
+    // Java implementation to check
+    // validity of the blockchain
+
+    // Function to check
+    // validity of the blockchain
+    public static Boolean isChainValid()
+    {
+	    Bloque currentBlock;
+	    Bloque previousBlock;
+
+	    // Iterating through
+	    // all the blocks
+	    for (int i = 1; i < blockchain.size(); i++) {
+
+		    // Storing the current block
+		    // and the previous block
+		    currentBlock = blockchain.get(i);
+		    previousBlock = blockchain.get(i - 1);
+
+            //Revisar si el hash almacenado en el bloque es correcto
+		    if (!currentBlock.darHash().equals(currentBlock.calcularHash())) {
+			        System.out.println("El hash actual es correcto.");
+			        return false;
+		    }
+
+		    // Revisar si el hash que aparece como previo es igual
+		    // al hash almacenado en el bloque previo
+		    if (!previousBlock.darHash().equals(currentBlock.darHashPrevio())) {
+			        System.out.println("El hash previo no es correcto");
+			        return false;
+		    }
+	}
+
+	// If all the hashes are equal
+	// to the calculated hashes,
+	// then the blockchain is valid
+	return true;
+}
+
+public static void printChain()
+{
+    for (int i = 0; i < blockchain.size(); i++) {
+        Bloque currentBlock = blockchain.get(i);
+        currentBlock.printBlock();
+    }    
+}
+
+public static void addBlock(Bloque newBlock) {
+    newBlock.minarBloque(dificultad);
+    blockchain.add(newBlock);
+
+}
+
+public static void main(String[] args)
+{
+		long start= System.currentTimeMillis();
+
+        System.out.println("Creando el bloque Genesis... ");
+        Bloque genesis = new Bloque(0,"Bloque Genesis", "0");
+        addBlock(genesis);
+
+        System.out.println("Adicionando el primer bloque ");
+        Bloque bloque1 = new Bloque(1,"Primer Bloque",genesis.darHash());
+        addBlock(bloque1);
+
+        System.out.println("\n Imprimiendo la cadena resultante: ");
+        printChain();
+        System.out.println("verificando validez de la cadena: " + isChainValid());
+		
+		long finish = System.currentTimeMillis();
+		long timeElapsed = finish - start ;
+		System.out.println("Tiempo ejecucion: " + timeElapsed +" ms");
+}
+}
+
+
